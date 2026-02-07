28bea1d441fa93927acbedefabce4fc8cece3743
ei5030 <77903574+ei5030@users.noreply.github.com>
2026-02-07 18:56:13 +0900

    fix: clean config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,14 +4,13 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   
-  // 通知の設定
-  reporter: [
-    ['line'], // コンソールに結果を表示
+ reporter: [
+    ['line'],
     [
       'playwright-slack-report',
       {
-        slackWebHookUrl: `https://hooks.slack.com/services/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64`,
-        sendResults: 'always', // 成功しても失敗しても通知する
+        slackWebHookUrl: 'https://hooks.slack.com/services/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',
+        sendResults: 'always',
       },
     ],
   ],
